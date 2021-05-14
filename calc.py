a4f381cc59f76ae923e903f76f1050792f5dbd22
🦄kembr🦄 <34753004+kembr@users.noreply.github.com>
2021-05-14 13:57:26 -0400

    summing x and y

--- calc.py
+++ calc.py
@@ -1,7 +1,13 @@
 import json
 
 with open("output.json") as jsonfile:
-    data = jsonfile.read()
+    data = json.load(jsonfile)
 
-# people = data["people"]
-print(data)
+hand_left_x = data["people"][0]["hand_left_keypoints_2d"][0]
+hand_left_y = data["people"][0]["hand_left_keypoints_2d"][1]
+
+added = hand_left_x + hand_left_y
+
+print(added)
+print(hand_left_x)
+print(hand_left_y)
