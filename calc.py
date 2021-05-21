eb7c0c54440ce111d0ffda294a7fdc09c07165dd
🦄kembr🦄 <34753004+kembr@users.noreply.github.com>
2021-05-21 12:30:56 -0400

    add note

--- calc.py
+++ calc.py
@@ -3,10 +3,12 @@ import json
 with open("output.json") as jsonfile:
     data = json.load(jsonfile)
 
-hand_left_x = data["people"][0]["hand_left_keypoints_2d"][0]
-hand_left_y = data["people"][0]["hand_left_keypoints_2d"][1]
+hand_left_x0 = data["people"][0]["hand_left_keypoints_2d"][0]
+hand_left_y0 = data["people"][0]["hand_left_keypoints_2d"][1]
 
-added = hand_left_x + hand_left_y
+added = hand_left_x0 + hand_left_y0
+
+# next we want to calculate the angles from (x0,y0) to (x1,y1)
 
 print(added)
 print(hand_left_x)
