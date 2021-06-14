62c46788fe4260108d1e31fcacf606cdc02b31b3
🦄kembr🦄 <34753004+kembr@users.noreply.github.com>
2021-06-14 11:15:15 -0400

    delete obsolete calc.py

--- calc.py
+++
@@ -1,9 +0,0 @@
-from functions import *
-
-folder = "arms_output"
-data = load_openpose(folder)
-
-# Extract the pose keypoints from the data
-data = [x["people"][0]["pose_keypoints_2d"] for x in data]
-# Split data into points
-data = [split_to_points(x) for x in data]
