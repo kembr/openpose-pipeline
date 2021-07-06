6ba54bd6a34568a3cd820a9024ca51a9f16d1b3a
Niklas Sheth <niklassheth@gmail.com>
2021-07-06 15:17:14 -0400

    Finish implementing functions

--- openposedemo.py
+++ openposedemo.py
@@ -1,9 +1,19 @@
 import os
 import subprocess
 
-file = __file__
-openposedir = os.path.abspath('openpose')
-video = os.path.abspath('arms.mp4')
-options = f'--video {video}'
-#subprocess.run([r'D:/Documents/openpose-pipeline/openpose/bin/OpenPoseDemo.exe', options], cwd=r'D:/Documents/openpose-pipeline/openpose', shell=False)
-subprocess.run([os.path.join(openposedir, 'bin/OpenPoseDemo.exe'), options], cwd=openposedir, shell=False)
+
+
+
+def openposedemo(options):
+    openposedir = os.path.abspath('openpose')
+    subprocess.run([os.path.join(openposedir, 'bin/OpenPoseDemo.exe'), *options], cwd=openposedir)
+
+
+def run_openpose(input_video, speedup=True):
+    video_name = os.path.split(input_video)[1].split('.')[0]
+    folder_path = os.path.abspath(os.path.join('output', video_name))
+    os.mkdir(folder_path)
+    json_path = os.path.abspath(os.path.join(folder_path, 'json'))
+    #os.mkdir(json_path)
+    openposedemo(['--video', os.path.abspath(input_video), '--write_json', json_path, '--write_video', os.path.join(folder_path ,video_name + '_saved.avi')] + (['--tracking', '1', '--number_people_max', '1'] if speedup else []))
+    return folder_path
